25c74e9f6ae5c22115bc9ad2c7658e89561334ed
lois <lois2.bentrari@epitech.eu>
2020-02-07 14:43:04 +0100

    callbakc code google and request token don't work

--- server/src/main/java/com/server/Area/Controllers/GoogleController.java
+++ server/src/main/java/com/server/Area/Controllers/GoogleController.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+
 public class GoogleController {
 
     @ApiModelProperty(notes = "Google's Token")
@@ -24,6 +31,13 @@ public class GoogleController {
     public GoogleController(String code, Connection c, PreparedStatement stmt) {
         System.out.println("JE suis sur la bonne route _____________---------------");
         System.out.println(code);
+        String url = "https://www.googleapis.com/oauth2/v4/token?code="+ code +"&client_id=377968007025-013sa07vehs51n1rau6qfmplp7esq964.apps.googleusercontent.com&client_secret=dXw6n2fh3lNh6URBVW_0P6xO&grant_type=authorization_code&redirect_uri=http://localhost:9090";
+        try {
+            JSONObject json = JsonReader.readJsonFromUrl(url);
+            System.out.println(json.toString());
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
 
     }
 

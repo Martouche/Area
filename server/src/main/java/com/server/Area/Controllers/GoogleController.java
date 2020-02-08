3c8b0430e84e0738fcc12d8b1983522fe1b05a49
lois <lois2.bentrari@epitech.eu>
2020-02-08 12:35:11 +0100

    connection google with getter acces token done

--- server/src/main/java/com/server/Area/Controllers/GoogleController.java
+++ server/src/main/java/com/server/Area/Controllers/GoogleController.java
@@ -19,8 +19,36 @@ import io.swagger.annotations.ApiModelProperty;
 import java.net.URL;
 import java.nio.charset.Charset;
 
-import org.json.JSONObject;
+//import org.json.JSONObject;
 import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.common.collect.ImmutableMap;
 
 
 public class GoogleController {
@@ -31,15 +59,85 @@ public class GoogleController {
     public GoogleController(String code, Connection c, PreparedStatement stmt) {
         System.out.println("JE suis sur la bonne route _____________---------------");
         System.out.println(code);
-        String url = "https://www.googleapis.com/oauth2/v4/token?code="+ code +"&client_id=377968007025-013sa07vehs51n1rau6qfmplp7esq964.apps.googleusercontent.com&client_secret=dXw6n2fh3lNh6URBVW_0P6xO&grant_type=authorization_code&redirect_uri=http://localhost:9090";
+        String clientId = "377968007025-013sa07vehs51n1rau6qfmplp7esq964.apps.googleusercontent.com";
+        String clientSecret = "dXw6n2fh3lNh6URBVW_0P6xO";
+        //String url = "https://www.googleapis.com/oauth2/v4/token?code="+ code +"&client_id=377968007025-013sa07vehs51n1rau6qfmplp7esq964.apps.googleusercontent.com&client_secret=dXw6n2fh3lNh6URBVW_0P6xO&grant_type=authorization_code&redirect_uri=http://localhost";
+
         try {
-            JSONObject json = JsonReader.readJsonFromUrl(url);
-            System.out.println(json.toString());
-        }catch(IOException e) {
-            e.printStackTrace();
+            String body = post("https://accounts.google.com/o/oauth2/token", ImmutableMap.<String, String>builder()
+                    .put("code", code)
+                    .put("client_id", clientId)
+                    .put("client_secret", clientSecret)
+                    .put("redirect_uri", "http://localhost:8080/oauth2/google")
+                    .put("grant_type", "authorization_code").build());
+
+            System.out.println(body);
+
+            JSONObject jsonObject = null;
+
+            // get the access token from json and request info from Google
+            try {
+                jsonObject = (JSONObject) new JSONParser().parse(body);
+            } catch (ParseException e) {
+                throw new RuntimeException("Unable to parse json " + body);
+            }
+
+            // google tokens expire after an hour, but since we requested offline access we can get a new token without user involvement via the refresh token
+            String accessToken = (String) jsonObject.get("access_token");
+        } catch (IOException e) {
+            System.out.println(e);
         }
 
+
+        // you may want to store the access token in session
+        //c.getSession().setAttribute("access_token", accessToken);
+
+
+        // get some info about the user with the access token
+        //String json = get(new StringBuilder("https://www.googleapis.com/oauth2/v1/userinfo?access_token=").append(accessToken).toString());
+
+        // now we could store the email address in session
+
+        // return the json of the user's basic info
+        //System.out.println(json);
+    }
+
+    // makes a GET request to url and returns body as a string
+    public String get(String url) throws ClientProtocolException, IOException {
+        return execute(new HttpGet(url));
+    }
+
+
+    public String post(String url, Map<String,String> formParameters) throws ClientProtocolException, IOException {
+        HttpPost request = new HttpPost(url);
+
+        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+
+        for (String key : formParameters.keySet()) {
+            nvps.add(new BasicNameValuePair(key, formParameters.get(key)));
+        }
+
+        request.setEntity(new UrlEncodedFormEntity(nvps));
+
+        return execute(request);
+    }
+
+    // makes request and checks response code for 200
+    private String execute(HttpRequestBase request) throws ClientProtocolException, IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = httpClient.execute(request);
+
+        HttpEntity entity = response.getEntity();
+        String body = EntityUtils.toString(entity);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new RuntimeException("Expected 200 but got " + response.getStatusLine().getStatusCode() + ", with body " + body);
+        }
+
+        return body;
     }
 
     public String getCode() { return code; }
+
+
 }
